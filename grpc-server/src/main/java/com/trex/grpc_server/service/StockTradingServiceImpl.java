1ccd4ad818b0f3e0a115e943e55a49bbaa977425
T-REX911 <thumuladhanujaka@outlook.com>
2025-11-03 22:44:55 +0530

    convert stock object in to stock response and return

--- grpc-server/src/main/java/com/trex/grpc_server/service/StockTradingServiceImpl.java
+++ grpc-server/src/main/java/com/trex/grpc_server/service/StockTradingServiceImpl.java
@@ -18,5 +18,14 @@ public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradin
     public void getStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
         String stockSymbol = request.getStockSymbol();
         Stock stockEntity = stockRepository.findByStockSymbol(stockSymbol);
+
+        StockResponse stockResponse = StockResponse.newBuilder()
+                .setStockSymbol(stockEntity.getStockSymbol())
+                .setPrice(stockEntity.getPrice())
+                .setTimestamp(stockEntity.getLastUpdated().toString())
+                .build();
+
+        responseObserver.onNext(stockResponse);
+        responseObserver.onCompleted();
     }
 }
