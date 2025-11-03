fd04cc811595b32ab829e845d726a6259a11a563
T-REX911 <thumuladhanujaka@outlook.com>
2025-11-03 22:39:41 +0530

    get stock symbol from req, get stock entity by stock sym from db

--- grpc-server/src/main/java/com/trex/grpc_server/service/StockTradingServiceImpl.java
+++ grpc-server/src/main/java/com/trex/grpc_server/service/StockTradingServiceImpl.java
@@ -3,14 +3,20 @@ package com.trex.grpc_server.service;
 import com.trex.grpc.StockRequest;
 import com.trex.grpc.StockResponse;
 import com.trex.grpc.StockTradingServiceGrpc;
+import com.trex.grpc_server.entity.Stock;
+import com.trex.grpc_server.repository.StockRepository;
 import io.grpc.stub.StreamObserver;
 import org.springframework.grpc.server.service.GrpcService;
 
 @GrpcService
 public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
 
+    private StockRepository stockRepository;
+
+
     @Override
     public void getStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
-        super.getStockPrice(request, responseObserver);
+        String stockSymbol = request.getStockSymbol();
+        Stock stockEntity = stockRepository.findByStockSymbol(stockSymbol);
     }
 }
