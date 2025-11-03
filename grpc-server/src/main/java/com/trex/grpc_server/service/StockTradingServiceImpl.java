ec8de4a91cf154f699a4192c0fed51e3282217c5
T-REX911 <thumuladhanujaka@outlook.com>
2025-11-03 22:11:35 +0530

    extend from StockTradingServiceImplBase and override getStockPrice

--- grpc-server/src/main/java/com/trex/grpc_server/service/StockTradingServiceImpl.java
+++ grpc-server/src/main/java/com/trex/grpc_server/service/StockTradingServiceImpl.java
@@ -1,4 +1,16 @@
 package com.trex.grpc_server.service;
 
-public class StockTradingServiceImpl {
+import com.trex.grpc.StockRequest;
+import com.trex.grpc.StockResponse;
+import com.trex.grpc.StockTradingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
+
+    @Override
+    public void getStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
+        super.getStockPrice(request, responseObserver);
+    }
 }
